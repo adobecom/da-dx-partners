ecc2cf6316fb24d8f5b56b6aa2e3571a49e33f9e
cod47516 <cod47516@adobe.com>
2025-06-25 09:21:40 +0200

    MWPW-172075 remove unused method

--- eds/scripts/personalizationUtils.js
+++ eds/scripts/personalizationUtils.js
@@ -1,4 +1,4 @@
-import { getPartnerDataCookieObject, hasProgramData, hasSalesCenterAccess } from './utils.js';
+import { getPartnerDataCookieObject, hasSalesCenterAccess } from './utils.js';
 import { PROGRAM } from '../blocks/utils/dxConstants.js';
 
 export const PERSONALIZATION_HIDE = 'personalization-hide';
@@ -22,27 +22,4 @@ export function processSalesAccess(el, programType) {
   if (!salesAccess) {
     element.classList.add(PERSONALIZATION_HIDE);
   }
-}
-
-export function processProfileItem(el, programType) {
-  if (hasProgramData(programType)) return;
-  el.classList.add(PERSONALIZATION_HIDE);
-
-  const profile = el.closest('.profile');
-  if (!profile) return;
-
-  const profileItems = profile.querySelectorAll('h5, p, a, hr');
-  const firstElement = Array.from(profileItems).find(item => !item.classList.contains(PERSONALIZATION_HIDE));
-  if (firstElement) {
-    firstElement.classList.add('no-section-title');
-  }
-
-  const sectionTitles = profile.querySelectorAll('h5');
-  sectionTitles.forEach((sectionTitle) => {
-    sectionTitle.classList.add(PERSONALIZATION_HIDE);
-  });
-
-  const divider = profile.querySelector('hr');
-  if (!divider) return;
-  divider.classList.add(PERSONALIZATION_HIDE);
 }
