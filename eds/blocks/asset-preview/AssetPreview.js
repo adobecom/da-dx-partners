4685fada5aba6cd0d66a1e6df895d00b74177f54
Sonja Popovic <cod55010@adobe.com>
2025-08-25 12:36:06 +0200

    MWPW-178097-remove test data

--- eds/blocks/asset-preview/AssetPreview.js
+++ eds/blocks/asset-preview/AssetPreview.js
@@ -50,32 +50,6 @@ constructor() {
     }).then((error) => {
        console.log('Asset doesnt exist on this instance ', error);
     })
-    const testAssetmetadata = {
-      "summary": "he document titled \"Desktop Deal Registration: A Step by Step Guide\" serves as a technical guide aimed at helping partners navigate the deal registration submission process through the Sales Centre/Adobe Partner Portal. It provides detailed instructions on creating and submitting new opportunities, along with key information about customer accounts and product details. The initial sections outline the steps to create a new opportunity, emphasizing adherence to Adobe's naming conventions and outlining mandatory information requirements for submission.\\n\\nSignificant dates mentioned include the preparation of the document based on FY20Q2 regional program guides, specifically noted as September 15, 2020. The guide features a detailed checklist for quick verification before deal submission, as well as a structured approach for entering customer and opportunity information, which includes notable requirements such as the industry classification and partner's sales activities. Throughout, the document emphasizes the importance of accuracy and compliance to ensure successful deal registration",
-      "types": [],
-      "aemPath": "/content/dam/solution/en/qatest/World Ocean Day Brochure.pdf",
-      "partnerLevel": [],
-      "description": "World Ocean Day Brochure",
-      "title": "World Ocean Day Brochure",
-      "fileType": "application/pdf",
-      // "url": "https://author.local.adobe.com/digitalexperience/preview/qatest/World%20Ocean%20Day%20Brochure.pdf",
-      // "url": "http://partners.stage.adobe.com/content/dam/solution/en/qatest/sample_1280x720_surfing_with_audio.mov",
-      "url": "http://partners.stage.adobe.com/content/dam/solution/en/qatest/CoreGuard%20Presentation.pptx",
-      "tags": [
-        "caas:content-type/guide",
-        "caas:products/adobe-analytics",
-        "caas:adobe-partners/collections/resources",
-        "caas:file-format/pdf",
-        "caas:audience/customer-facing",
-        "caas:audience/partners-facing",
-      ],
-      "size": "8353",
-      "createdDate": "2025-06-24T12:04:44.129Z",
-
-      "thumbnailUrl": "https://partners.stage.adobe.com/content/dam/solution/en/qatest/CoreGuard%20Presentation.pdf/jcr:content/renditions/cq5dam.thumbnail.319.319.png",
-    };
-   await this.setData(testAssetmetadata);
-
   }
   async setData(assetMetadata) {
     const allTags = await fetch(CAAS_TAGS_URL).then((res) => res.json());
@@ -170,7 +144,6 @@ constructor() {
   }
 
   getFileTypeFromTag() {
-    console.log(this.fileFormatTags);
     // we should always have only one file format tag since it is added based on file type
     // or we should use this.fileType but this has some ugly values (see
     // https://git.corp.adobe.com/wcms/gravity/blob/develop/app-configuration/core/src/main/java/com/adobe/wcm/configuration/utils/CaaSContentDXUtils.java#L52
