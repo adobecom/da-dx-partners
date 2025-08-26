79ce8a379e4c38163c83a46f7d715cc76b4b9075
Sonja Popovic <cod55010@adobe.com>
2025-08-26 18:26:28 +0200

    MWPW-178097-local dev changes

--- eds/blocks/asset-preview/AssetPreview.js
+++ eds/blocks/asset-preview/AssetPreview.js
@@ -1,6 +1,8 @@
-import {CAAS_TAGS_URL, getLibs} from "../../scripts/utils.js";
+import {CAAS_TAGS_URL, getLibs, getPartnerDataCookieValue} from "../../scripts/utils.js";
 import {assetPreviewStyles} from "./AssetPreviewStyles.js";
 import {transformCardUrl} from "../utils/utils.js";
+import {PARTNER_LEVEL, PX_PARTNER_LEVELS} from "../utils/dxConstants.js";
+
 const DEFAULT_BACKGROUND_IMAGE_PATH = '/content/dam/solution/en/images/card-collection/sample_default.png';
 
 const miloLibs = getLibs();
@@ -9,9 +11,7 @@ export default class AssetPreview extends LitElement {
 static styles = [
   assetPreviewStyles
     ];
-constructor() {
-  super();
-}
+
   static properties = {
     blockData: { type: Object },
     title: { type: String },
@@ -21,67 +21,116 @@ constructor() {
     url: { type: String },
     thumbnailUrl: { type: String },
     tags: { type: Array },
+    allAssetTags: { type: Array },
     ctaText: { type: String },
     returnUrl: {type: String},
     createdDate: {type: Date},
-    audience: {type: Array},
-    types: {type: Array},
+    assetExist: {type: Boolean},
   };
+
+constructor() {
+  super();
+  console.log('in constructor');
+  this.assetExist = false;
+  this.tags = [];
+  this.allAssetTags = []
+  this.allCaaSTags = [];
+}
   async connectedCallback() {
+    console.log('in connectedcallback');
     super.connectedCallback();
-    await this.getAssetMetadata();
+    // await this.getAssetMetadata();
+    const testAssetmetadata = {
+      "summary": "he document titled \"Desktop Deal Registration: A Step by Step Guide\" serves as a technical guide aimed at helping partners navigate the deal registration submission process through the Sales Centre/Adobe Partner Portal. It provides detailed instructions on creating and submitting new opportunities, along with key information about customer accounts and product details. The initial sections outline the steps to create a new opportunity, emphasizing adherence to Adobe's naming conventions and outlining mandatory information requirements for submission.\\n\\nSignificant dates mentioned include the preparation of the document based on FY20Q2 regional program guides, specifically noted as September 15, 2020. The guide features a detailed checklist for quick verification before deal submission, as well as a structured approach for entering customer and opportunity information, which includes notable requirements such as the industry classification and partner's sales activities. Throughout, the document emphasizes the importance of accuracy and compliance to ensure successful deal registration",
+      "types": [],
+      "aemPath": "/content/dam/solution/en/qatest/World Ocean Day Brochure.pdf",
+      "partnerLevel": [],
+      "description": "World Ocean Day Brochure",
+      "title": "World Ocean Day Brochure",
+      "fileType": "application/pdf",
+      // "url": "https://author.local.adobe.com/digitalexperience/preview/qatest/World%20Ocean%20Day%20Brochure.pdf",
+      // "url": "http://partners.stage.adobe.com/content/dam/solution/en/qatest/sample_1280x720_surfing_with_audio.mov",
+      "url": "http://partners.stage.adobe.com/content/dam/solution/en/qatest/CoreGuard%20Presentation.pptx",
+      "tags": [
+        "caas:content-type/guide",
+        "caas:products/adobe-analytics",
+        "caas:adobe-partners/collections/resources",
+        "caas:file-format/pdf",
+        "caas:audience/customer-facing",
+        "caas:audience/partners-facing",
+      ],
+      "size": "8353",
+      "createdDate": "2025-06-24T12:04:44.129Z",
+
+      "thumbnailUrl": "https://partners.stage.adobe.com/content/dam/solution/en/qatest/CoreGuard%20Presentation.pdf/jcr:content/renditions/cq5dam.thumbnail.319.319.png",
+    };
+    this.allCaaSTags = await fetch(CAAS_TAGS_URL).then((res) => res.json());
+    await this.setData(testAssetmetadata);
   }
 
   async getAssetMetadata() {
-    // for domain we use what is in  window.location.href (this assumes that on cards we have partners.stage.adobe.com or partners.adobe.com
+
+  console.log('in get assetmetadat');
+  // for domain we use what is in  window.location.href (this assumes that on cards we have partners.stage.adobe.com or partners.adobe.com
     // on prod caas index we would have only have prod assets, so asset metadata  would always be found on prod
     // for stage, we will display also some assets from qa01 or dev02, but will always fetch asset metadata from stage
     // so we should delete assets from lower env if they make us problem on stage
-    const mappedAssetUrl = this.getRealAssetUrl();
+    // const mappedAssetUrl = this.getRealAssetUrl();
+const mappedAssetUrl = 'https://partners.stage.adobe.com/content/dam/solution/en/qatest/Webprogramiranje.zip.assetmetadata.json';
+    try {
+      const resp = await fetch(mappedAssetUrl).then(async (res) => {
+        if(res && res.status === 200 ) {
+          console.log('dsafdfd');
 
-    await fetch(mappedAssetUrl).then(async (res) => {
-      if(res.status === 200 ) {
-        const assetMetadata = await res.json();
-        await this.setData(assetMetadata);
-      } else {
-        console.log(res.status);
-      }
-
-    }).then((error) => {
-       console.log('Asset doesnt exist on this instance ', error);
-    })
+          const assetMetadata = await res.json();
+          await this.setData(assetMetadata);
+        }
+      }).then((error) => {
+        console.log('Asset doesnt exist on this instance ', error);
+      });
+    } catch (e) {
+    }
   }
   async setData(assetMetadata) {
-    const allTags = await fetch(CAAS_TAGS_URL).then((res) => res.json());
+    console.log('assetmetadata in setdata', assetMetadata);
     this.title = assetMetadata.title;
     this.summary = assetMetadata.summary;
     this.description = assetMetadata.description;
     this.fileType = assetMetadata.fileType;
     this.url = assetMetadata.url;
     this.thumbnailUrl = assetMetadata.thumbnailUrl;
-    this.tags = this.getTagsDisplayValues(allTags, assetMetadata.tags);
+    this.tags = assetMetadata.tags ? this.getTagsDisplayValues(this.allCaaSTags, assetMetadata.tags) : [];
+    this.allAssetTags = assetMetadata.tags;
     this.ctaText = assetMetadata.ctaText;
     this.size = this.getSizeInMb(assetMetadata.size);
-    this.createdDate = assetMetadata.createdDate? new Date(assetMetadata.createdDate).toLocaleDateString('en-US') : null;
-    this.audienceTags = this.getTagsFromRoot(assetMetadata.tags, allTags, 'caas:audience');
-    this.fileFormatTags = this.getTagsFromRoot(assetMetadata.tags, allTags, 'caas:file-format');
+    this.createdDate = (() => {
+      if (!assetMetadata.createdDate) return null;
+      const date = new Date(assetMetadata.createdDate);
+      return !isNaN(date.getTime()) ? date.toLocaleDateString('en-US') : null;
+    })();
+    console.log('assetmetadata tasgs', assetMetadata.tags);
+    this.audienceTags = assetMetadata.tags ? this.getTagChildTagsObjects(assetMetadata.tags, this.allCaaSTags, 'caas:audience') : [];
+    this.fileFormatTags = assetMetadata.tags ? this.getTagChildTagsObjects(assetMetadata.tags, this.allCaaSTags, 'caas:file-format'): [];
+    this.assetExist = true;
+    console.log('ass ex in set data', this.assetExist);
   }
    getRealAssetUrl() {
     return window.location.href.replace('/digitalexperience/preview/', '/content/dam/solution/en/').concat('.assetmetadata.json');
   }
   render() {
+    console.log('this aset ex in redner', this.assetExist);
     const isVideo = this.fileFormatTags && this.fileFormatTags[0].tagId === 'caas:file-format/video';
-    return html` ${this.title 
+    return html`${this.assetExist 
       ? html`
         <div class="asset-preview-block-container">
         <div class="asset-preview-block-header">Asset detail: ${this.title} ${this.getFileTypeFromTag()}</div>
       <div class="asset-preview-block-details ">
         <div class="asset-preview-block-details-left">
           <p><span class="asset-preview-block-details-left-label">Date: </span>${this.createdDate}</p>
-          <p><span class="asset-preview-block-details-left-label">Audience: </span>${this.getTagsTitles(this.audienceTags)}</p>
+          <p><span class="asset-preview-block-details-left-label">Audience: </span>${this.getTagsTitlesString(this.audienceTags)}</p>
           <p><span class="asset-preview-block-details-left-label">Summary: </span>${isVideo ? this.description : this.summary || this.description}</p>
-          <p><span class="asset-preview-block-details-left-label">Type: </span>${this.getTagsTitles(this.fileFormatTags)}</p>
-          <p><span class="asset-preview-block-details-left-label">Tags: </span>${this.getTagsTitles(this.tags)}</p>
+          <p><span class="asset-preview-block-details-left-label">Type: </span>${this.getTagsTitlesString(this.fileFormatTags)}</p>
+          <p><span class="asset-preview-block-details-left-label">Tags: </span>${this.getTagsTitlesString(this.tags)}</p>
           <p><span class="asset-preview-block-details-left-label">Size: </span class="bold">${this.size}</p>
         </div>
         <div class="asset-preview-block-details-right"
@@ -91,11 +140,13 @@ constructor() {
         >
         </div>
       </div>
-        <div class="asset-preview-block-actions">
-          ${!this.isPreviewEnabled(this.getFileTypeFromTag()) ? html` <button class="outline" ><a  target="_blank" rel="noopener noreferrer" href="${this.url}">View</a></button>` : ''}
-         <button class="filled""><a href="${this.url}">Download</a></button>
-          <a class="link" href="${this.returnUrl}">Back to previous</a>
-        </div>
+          ${!this.isRestrictedAssetForUser() ?
+    html` <div class="asset-preview-block-actions">
+      ${!this.isPreviewEnabled(this.getFileTypeFromTag()) ? html` <button class="outline" ><a  target="_blank" rel="noopener noreferrer" href="${this.url}">View</a></button>` : ''}
+      <button class="filled""><a href="${this.getDownloadUrl()}">Download</a></button>
+      <a class="link" href="${this.returnUrl}">Back to previous</a>
+    </div>` : ''}
+       
         </div>
       `: 
       html`<div class="asset-preview-block-container">
@@ -116,9 +167,11 @@ constructor() {
   }
 
    getTagsDisplayValues(allTags, tags) {
+   console.log('tags', tags);
+
     const tagsArray = [];
     tags.forEach((tag) => {
-      const tagObject = this.findTagByPath( allTags.namespaces.caas.tags, tag) || {tagId: tag, title: tag};
+      const tagObject = this.findTagByPath( this.allCaaSTags.namespaces.caas.tags, tag) || {tagId: tag, title: tag};
       tagsArray.push({tagId: tag, title: tagObject.title});
     });
     return tagsArray;
@@ -138,9 +191,15 @@ constructor() {
     return caasPointer || null;
   }
 
-  getTagsFromRoot(tags, allTags, rootTag) {
+  getTagChildTagsObjects(tags, allTags, rootTag) {
     const filteredTags = tags.filter(t => t.startsWith(rootTag));
-    return this.getTagsDisplayValues(allTags, filteredTags);
+
+    const tagsArray = [];
+    filteredTags.forEach((tag) => {
+      const tagObject = this.findTagByPath( this.allCaaSTags.namespaces.caas.tags, tag) || {tagId: tag, title: tag};
+      tagsArray.push({tagId: tag, title: tagObject.title});
+    });
+    return tagsArray;
   }
 
   getFileTypeFromTag() {
@@ -152,7 +211,21 @@ constructor() {
     else return '';
   }
 
-  getTagsTitles(tags) {
+  getTagsTitlesString(tags) {
     return tags?.map((tag,i) => `${tag.title}${this.tags.length-1 > i ? ', ': ''}`);
+  }
+
+  getDownloadUrl() {
+    return this.url?.replace("/digitalexperience/preview/", "/digitalexperience-assets/");
+  }
+
+  isRestrictedAssetForUser() {
+    const assetPxPartnerLevels = this.getTagChildTagsObjects(this.allAssetTags, this.allCaaSTags,'caas:adobe-partners/px/partner-level').map(t => t.id);
+    if (!assetPxPartnerLevels.length ||
+      assetPxPartnerLevels.includes(PX_PARTNER_LEVELS.PUBLIC) ||
+      assetPxPartnerLevels.includes(PARTNER_LEVEL)) {
+      return false;
+    }
+    return true;
   }
 }
--- eds/blocks/utils/dxConstants.js
+++ eds/blocks/utils/dxConstants.js
@@ -5,3 +5,12 @@ export const RT_SEARCH_ACTION_PATH = '/api/v1/web/da-dx-partners-runtime/search-
 export const DX_PROGRAM_TYPE = 'spp';
 
 export const PARTNER_LEVEL = getPartnerDataCookieValue('level', DX_PROGRAM_TYPE);
+
+export const PX_PARTNER_LEVELS = Object.freeze( {
+  PUBLIC: 'caas:adobe-partners/px/partner-level/public',
+  COMMUNITY: 'caas:adobe-partners/px/partner-level/community',
+  SILVER: 'caas:adobe-partners/px/partner-level/silver',
+  GOLD: 'caas:adobe-partners/px/partner-level/gold',
+  PLATINUM: 'caas:adobe-partners/px/partner-level/platinum',
+});
+
