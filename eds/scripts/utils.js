6873ba9191a26c5118b3facc74b3208879806019
cod47516 <cod47516@adobe.com>
2025-12-08 10:44:11 +0100

    partnership progress block

--- eds/scripts/utils.js
+++ eds/scripts/utils.js
@@ -377,6 +377,7 @@ export function updateIMSConfig() {
     }
 
     window.adobeIMS.adobeIdData.redirect_uri = targetUrl.toString();
+    window.dispatchEvent(new Event('dxp:imsReady'));
   }, 500);
 }
 
