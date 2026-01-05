40e2c170728c9184993627df952b9f1a6c38592a
Slobodanka <31386866+slobodankajovicic@users.noreply.github.com>
2026-01-05 12:00:14 +0100

    MWPW-181689: MAPP - Multiple Card collections on one page, filters will be applied to multiple card collections

--- eds/components/PartnerCards.js
+++ eds/components/PartnerCards.js
@@ -675,6 +675,9 @@ export default class PartnerCards extends LitElement {
   additionalResetActions() {}
 
   handleSearchAction() {
+    // If Card Collection doesn't have filters enabled, do not apply search from URL search query param
+    if (this.blockData.filters.length === 0) return;
+
     // eslint-disable-next-line max-len
     this.cards = this.allCards.filter((card) => card.contentArea?.title.toLowerCase().includes(this.searchTerm)
       || card.contentArea?.description.toLowerCase().includes(this.searchTerm));
