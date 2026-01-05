fbe91924deeae4e66da6d3af41e2d3f113fdf8e2
cod17828 <cod17828@adobe.com>
2026-01-05 16:07:17 +0100

    adding yukon-chat TODO: remove svg-s from codebase adn upload them to da

--- eds/scripts/utils.js
+++ eds/scripts/utils.js
@@ -44,6 +44,8 @@ export const prodHosts = [
   'partnerspreview.adobe.com',
 ];
 
+export const aemPublish = prodHosts.includes(window.location.host) ? 'https://partners.adobe.com' : 'https://partners.stage.adobe.com';
+
 /*
  * ------------------------------------------------------------
  * Edit above at your own risk.
