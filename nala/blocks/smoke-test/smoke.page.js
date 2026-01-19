90df4b4ac7afb74d1d70152cc9344a5296054a55
Saska <kakucasaska@gmail.com>
2026-01-19 14:03:26 +0100

    smoke-tests-v1

--- nala/blocks/smoke-test/smoke.page.js
+++ nala/blocks/smoke-test/smoke.page.js
@@ -16,6 +16,45 @@ export default class SmokeTest {
     this.joinNowLinkTP = page.locator('a[href*="/technologyprogram/experiencecloud/registration.html"]');
     this.joinNowLinkAR = page.locator('a[href*="/na/channelpartners/enrollment/"]');
     this.footer = page.locator('.global-footer');
+    this.becomeAPartnerButton = page.getByRole('link', { name: 'Become a partner' });
+    this.signInButton = page.locator('.feds-signIn');
+    this.findAPartnerButton = page.getByRole('link', { name: 'Find a partner' });
+    this.profileIconButton = page.locator('.feds-profile-button');
+    this.emailField = page.locator('#EmailPage-EmailField');
+    this.emailPageContinueButton = page.locator('//button[@data-id="EmailPage-ContinueButton"]');
+    this.passwordField = page.locator('#PasswordPage-PasswordField');
+    this.passwordPageContinueButton = page.locator('//button[@data-id="PasswordPage-ContinueButton"]');
+    this.saleCenterButton = page.locator('a[href*="/digitalexperience/m/salescenter/"]:has(img[src*="handshake.svg"])');
+    this.homeButton = page.locator('a[href*="digitalexperience/home/"]:has(img[src*="home.svg"])');
+    this.searchField = page.getByRole('searchbox', { name: 'Search' });
+    this.searchAllResults = page.getByRole('button', { name: 'All', exact: true });
+    this.productFilter = page.getByLabel('Products');
+    this.productFilterPanel = page.getByRole('list').filter({ hasText: 'Adobe Advertising Cloud Adobe' });
+    this.productFilterCheckbox = page.getByRole('checkbox', { name: 'Adobe Advertising Cloud' });
+    this.loader = page.locator('.progress-circle-wrapper');
+    this.feedbackButton = page.locator('.feedback-mechanism');
+    this.feedbackTitle = page.locator('.feedback-title');
+    this.feedbackTextArea = page.locator('.feedback-textarea');
+    this.feedbackSendButton = page.locator('.feedback-dialog-button.cta');
+    this.feedBackStars3 = page.locator('sp-action-button[data-rating="3"]');
+    this.collectionBlock = page.locator('#explore-all-related-product-assets');
+    this.cardsResults = page.locator('.partner-cards-cards-results');
+    this.firstFilterButton = page.locator('.filter .filter-header').first();
+    this.firstFilterList = page.locator('.filter').first().locator('.filter-list');
+    this.firstFilterCheckbox = page.locator('.filter').first().locator('sp-checkbox').first();
+  }
+
+  async smokeSignIn(page, baseURL, partnerLevel) {
+    const isProduction = baseURL.includes('partners.adobe.com');
+    const emailData = isProduction ? process.env.IMS_EMAIL_PROD : process.env.IMS_EMAIL;
+    const emailPart = emailData.split(';');
+    const emailEntry = emailPart.find((pair) => pair.startsWith(partnerLevel));
+    const email = emailEntry ? emailEntry.split(':')[1] : null;
+    await page.waitForLoadState('domcontentloaded');
+    await this.emailField.fill(email);
+    await this.emailPageContinueButton.click();
+    await this.passwordField.fill(process.env.IMS_PASS);
+    await this.passwordPageContinueButton.click();
   }
 
   async verifyStatusCode(url) {
