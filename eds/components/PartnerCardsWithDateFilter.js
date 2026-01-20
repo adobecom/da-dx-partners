709527b8fe1c2e3498738f5b87d8737023c796e7
Dejan Bozic <2dmsoftware@gmail.com>
2026-01-20 13:06:39 +0100

    MWPW-186405: add condition when date filter is not present

--- eds/components/PartnerCardsWithDateFilter.js
+++ eds/components/PartnerCardsWithDateFilter.js
@@ -151,6 +151,7 @@ export default class PartnerCardsWithDateFilter extends PartnerCards {
   }
 
   additionalResetActions() {
+    if(!this.blockData.dateFilter) return;
     this.initDateTags(this.blockData.dateFilter.tags);
   }
 
