0483c1408b01e53db8d8ef219d48dd0f6ca31964
Ben Zahler <ben@hitthecode.com>
2026-02-06 08:24:45 +0100

    MWPW-187562: handle agreement already accepted case

--- eds/scripts/partnerAgreement.js
+++ eds/scripts/partnerAgreement.js
@@ -124,7 +124,7 @@ async function handleAgreement(action) {
             }
             return responseJson.terms?.[0];
         } else {
-          //error code "41012" means agreement already accepted
+            //error code "41012" means agreement already accepted
             if (responseJson.errorCode && responseJson.errorCode !== '41012') {
                 console.error(`Accepting partner agreement failed! Error code: ${responseJson.errorCode}`);
                 return false;
