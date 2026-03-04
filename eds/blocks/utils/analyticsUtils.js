bf3912cb966b4bade2fbb53192c00f00875e1a28
Sonja Popovic <cod55010@adobe.com>
2026-03-04 11:46:49 +0100

    MWPW-189208-fire event on card click

---
+++ eds/blocks/utils/analyticsUtils.js
@@ -0,0 +1,29 @@
+const customEventName = 'aep:TrackEvent';
+const appName = 'experience-hub';
+export const eventTypeSearch = 'search';
+export const eventTypeLinkClick = 'linkClick';
+
+function createCustomEvent(eventType, metadata) {
+  return new CustomEvent(customEventName, {
+    detail: {
+      appName,
+      eventType,
+      timestamp: Date.now(),
+      metadata,
+      xdm: {},
+    },
+  });
+}
+export function dispatchCustomEventOnLinkClick(e, link, linkText) {
+  window.dispatchEvent(
+    createCustomEvent(
+      eventTypeLinkClick,
+      // todo check for linkText what value they need
+      { link, linkText },
+    ),
+  );
+}
+
+export function dispatchCustomEventOnSearch() {
+  console.log('');
+}
