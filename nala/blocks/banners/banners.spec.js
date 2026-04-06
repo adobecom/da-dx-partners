880eb6296355af1d1d0e1f18ba5e6d7e9bacee34
Saska <kakucasaska@gmail.com>
2026-04-06 10:04:22 +0200

    bctq-banner-test

---
+++ nala/blocks/banners/banners.spec.js
@@ -0,0 +1,32 @@
+export default {
+  FeatureName: 'DX Banners',
+  features: [
+    {
+      tcid: '1',
+      name: '@banners-platinum-user-anyversery-date-in-next90-days',
+      path: '/digitalexperience/drafts/automation/regression/public-page',
+      tags: '@da-dx-banners @regression @circleCi',
+      data: {
+        partnerData: {
+            partnerPortal: 'DXP',
+            accessType: '["Billing Admin","Sales Center Admin","Admin"]',
+            complianceStatus: 'Completed',
+            daysToComplianceExpiry: 65,
+            designationType: '["Legal and Compliance"]',
+            email: 'yugo-test+dx-stage-platinum@adobetest.com',
+            isAdmin: 'true',
+            latestAgreementAccepted: 'true',
+            latestAgreementAcceptedVersion: 'Feb 6, 2026',
+            primaryBusiness: '["Solution"]',
+            primaryContact: 'true',
+            primaryJobRole: 'Product Management',
+            purchasedPartnerLevel: 'Platinum',
+            salesCenterAccess: 'true',
+            status: 'MEMBER',
+            partnerLevel: 'Platinum',
+          },
+          completeComplianceButtonLink: '/digitalexperience/m/manageprofile/companyinfo/information?open=compliance',
+      },
+    },
+  ],
+};
