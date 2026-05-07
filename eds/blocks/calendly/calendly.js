9224940fb01352564594bcbcd5b2a3dd4963ad54
Sonja Popovic <cod55010@adobe.com>
2026-05-07 13:03:58 +0200

    MWPW-191308-calendly ui integration

--- eds/blocks/calendly/calendly.js
+++ eds/blocks/calendly/calendly.js
@@ -23,7 +23,26 @@ function getLinkValue(columns) {
   if (authoredAnchor) return authoredAnchor.href;
   return columns[1].textContent.trim();
 }
-
+function isCalendlyEvent(e) {
+  return e.origin === 'https://calendly.com' && e.data?.event?.startsWith('calendly.');
+}
+function trackCalendlyEvent(e) {
+  if (!isCalendlyEvent(e)) return;
+  const { event, payload } = e.data;
+  // send to analytics here
+  console.log('[Calendly]', event, payload);
+  // todo send req to runtime to update crm data that event is scheduled, example:
+  // Calendly Event: calendly.event_scheduled
+  //
+  // {
+  //   "event": {
+  //   "uri": "https://api.calendly.com/scheduled_events/ef8c7e60-6055-407b-9f29-c5436dcf3285"
+  // },
+  //   "invitee": {
+  //   "uri": "https://api.calendly.com/scheduled_events/ef8c7e60-6055-407b-9f29-c5436dcf3285/invitees/7cf8da55-9460-4509-b56a-24cd9cb30ad1"
+  // }
+  // }
+}
 function initCalendly(link, parentElement) {
   window.Calendly.initInlineWidget({
     url: link,
@@ -52,7 +71,10 @@ export default async function init(el) {
 
   try {
     await loadCalendlyScript();
+    // todo if calendly link is invalid, it will show calendly not found page ... is that acceptable ?
+    // validating url before init would affect performance
     initCalendly(schedulingLink, calendlyEmbed);
+    window.addEventListener('message', trackCalendlyEvent);
   } catch (e) {
     // eslint-disable-next-line no-console
     console.error('Calendly widget failed to load', e);
