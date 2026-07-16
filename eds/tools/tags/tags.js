80063102e92a781d792e15b5e06cb3ffb5d17517
Ben Zahler <ben@hitthecode.com>
2026-07-16 11:23:16 +0200

    MWPW-198719: implement tests, split tags.js

--- eds/tools/tags/tags.js
+++ eds/tools/tags/tags.js
@@ -1,39 +1,7 @@
 /* eslint-disable import/no-unresolved */
 import DA_SDK from 'https://da.live/nx/utils/sdk.js';
 import './tag-browser.js';
-import TAG_DATA from './aem-tags.json' with { type: 'json' };
-
-function getNodeAtSegments(tree, segments) {
-  return segments.reduce((node, seg) => ((node && node[seg]) ? node[seg] : null), tree);
-}
-
-async function getTags(path) {
-  const segments = path.split('/').filter(Boolean);
-  const activeTag = segments.join('/');
-  const node = getNodeAtSegments(TAG_DATA, segments);
-  if (!node) {
-    // eslint-disable-next-line no-console
-    console.error(`No tag node found at path "${path}" (segments: ${JSON.stringify(segments)})`);
-    return null;
-  }
-
-  const tags = Object.keys(node).reduce((acc, key) => {
-    if (node[key]['jcr:primaryType'] === 'cq:Tag') {
-      acc.push({
-        path: `${path}/${key}`,
-        activeTag,
-        name: key,
-        title: node[key]['jcr:title'] || key,
-        details: node[key],
-      });
-    }
-    return acc;
-  }, []);
-
-  return tags;
-}
-
-const getRootTags = async () => getTags('').catch(() => null);
+import { getTags, getRootTags } from './tag-data.js';
 
 function showError(message, link = null) {
   const mainElement = document.body.querySelector('main');
