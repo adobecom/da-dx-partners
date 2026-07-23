3eaa7961f477d24fa653aa8be21cdae696c0dcaa
Sonja Popovic <cod55010@adobe.com>
2026-07-23 11:49:40 +0200

    MWPW-195867-improvement

--- eds/blocks/calendly/calendly.js
+++ eds/blocks/calendly/calendly.js
@@ -18,7 +18,7 @@ let toastStyleLoaded = false;
 
 const CALENDLY_ERROR_TOAST = { toastNegative: 'Unable to save your booking. Please contact support if the issue persists.' };
 
-const CALENDLY_DOUBLE_BOOKING_TOAST = { toastNegative: 'Calendly event was already booked by another admin.' };
+const CALENDLY_DOUBLE_BOOKING_TOAST = { toastNegative: 'A session is already scheduled for your account. Our team will reach out to help coordinate your bookings.' };
 
 const delay = (ms) => new Promise((resolve) => {
   setTimeout(resolve, ms);
